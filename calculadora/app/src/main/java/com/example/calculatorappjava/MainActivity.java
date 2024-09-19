d1ed6a2865c4d8006ae7985427927e7dbbb3c28e
DanielJava <danieledgar.caballerohu@elorrieta-errekamari.com>
2024-09-19 18:09:30 +0200

    se ha añadido la parte funcional de la calculadora falta que se guarde el estado al rotar la pantalla

--- calculadora/app/src/main/java/com/example/calculatorappjava/MainActivity.java
+++ calculadora/app/src/main/java/com/example/calculatorappjava/MainActivity.java
@@ -10,22 +10,20 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private boolean operationDone = false;
     private TextView textView = null;
-    private TextView textError = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textError = (TextView) findViewById(R.id.textView);
-        textError.setVisibility(View.GONE);
-        textView = (TextView) findViewById(R.id.textView2);
-
+        View v1 = findViewById(R.id.textView2);
+        textView = (TextView) v1;
+        textView.setText("");
         Button button1 = (Button) findViewById(R.id.button1);
-        button1.setOnClickListener(buton1());
+        button1.setOnClickListener(this);
         Button button2 = (Button) findViewById(R.id.button2);
-        button2.setOnClickListener(buton2());
+        button2.setOnClickListener(this);
         Button button3 = (Button) findViewById(R.id.button3);
-        button3.setOnClickListener(buton3());
+        button3.setOnClickListener(this);
         Button button4 = (Button) findViewById(R.id.button4);
         button4.setOnClickListener(this);
         Button button5 = (Button) findViewById(R.id.button5);
@@ -57,33 +55,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonDivi.setOnClickListener(this);
         Button buttonResult = (Button) findViewById(R.id.button20);
         buttonResult.setOnClickListener(this);
-        
+
     }
 
     @Override
     public void onClick(View v) {
-        try{
-            if (operationDone) {
-                textView.setText( "" );
-                operationDone = false;
-            }
+//Esta funcion se encarga de añadir un valor segun el boton que se precione
+            if(v.getId() == R.id.button1) textView.append("1");
+            else if(v.getId() == R.id.button2) textView.append("2");
+            else if(v.getId() == R.id.button3) textView.append("3");
+            else if(v.getId() == R.id.button4) textView.append("4");
+            else if(v.getId() == R.id.button5) textView.append("5");
+            else if(v.getId() == R.id.button6) textView.append("6");
+            else if(v.getId() == R.id.button7) textView.append("7");
+            else if(v.getId() == R.id.button8) textView.append("8");
+            else if(v.getId() == R.id.button9) textView.append("9");
+            else if(v.getId() == R.id.button19) textView.append("0");
+            else if(v.getId() == R.id.button31) textView.append(".");
+            else if(v.getId() == R.id.button28) textView.setText("");
+            else if(v.getId() == R.id.button29) textView.setText("");
+            else if(v.getId() == R.id.button26) textView.append("+");
+            else if(v.getId() == R.id.button27) textView.append("-");
+            else if(v.getId() == R.id.button25) textView.append("x");
+            else if(v.getId() == R.id.button23) textView.append("/");
+            else if(v.getId() == R.id.button) textView.append("²");
+            else if(v.getId() == R.id.button10) textView.append("√");
+            else if(v.getId() == R.id.button11) textView.append("(");
+            else if(v.getId() == R.id.button12) textView.append(")");
+            else if(v.getId() == R.id.button13) textView.append("%");
+            else if(v.getId() == R.id.button20) textView.append("");
 
-        } catch (Exception e){
-            textError.setVisibility(View.VISIBLE);
-            textView.setText("");
-        }
+
+
     }
 
-    public View.OnClickListener buton1 (){
-        textView.setText(textView.getText().toString() + "1");
-        return null;
-    }
-    public View.OnClickListener buton2 (){
-        textView.setText(textView.getText().toString() + "2");
-        return null;
-    }
-    public View.OnClickListener buton3 (){
-        textView.setText(textView.getText().toString() + "3");
-        return null;
-    }
+
 }
