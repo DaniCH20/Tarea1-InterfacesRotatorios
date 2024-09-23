91f5d5190a4afe2cb229b2338e5b9119499fc960
DanielJava <danieledgar.caballerohu@elorrieta-errekamari.com>
2024-09-23 20:40:36 +0200

    se ha intentado añadir el guardado de estados pero no ha funcionado habra que seguir investigando

--- calculadora/app/src/main/java/com/example/calculatorappjava/MainActivity.java
+++ calculadora/app/src/main/java/com/example/calculatorappjava/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.calculatorappjava;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    private boolean operationDone = false;
-    private TextView textView = null;
+import org.jetbrains.annotations.NotNull;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView textView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +21,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         View v1 = findViewById(R.id.textView2);
         textView = (TextView) v1;
-        textView.setText("");
+
         Button button1 = (Button) findViewById(R.id.button1);
         button1.setOnClickListener(this);
         Button button2 = (Button) findViewById(R.id.button2);
@@ -57,37 +61,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonResult.setOnClickListener(this);
 
     }
-
     @Override
     public void onClick(View v) {
+
 //Esta funcion se encarga de añadir un valor segun el boton que se precione
-            if(v.getId() == R.id.button1) textView.append("1");
-            else if(v.getId() == R.id.button2) textView.append("2");
-            else if(v.getId() == R.id.button3) textView.append("3");
-            else if(v.getId() == R.id.button4) textView.append("4");
-            else if(v.getId() == R.id.button5) textView.append("5");
-            else if(v.getId() == R.id.button6) textView.append("6");
-            else if(v.getId() == R.id.button7) textView.append("7");
-            else if(v.getId() == R.id.button8) textView.append("8");
-            else if(v.getId() == R.id.button9) textView.append("9");
-            else if(v.getId() == R.id.button19) textView.append("0");
-            else if(v.getId() == R.id.button31) textView.append(".");
-            else if(v.getId() == R.id.button28) textView.setText("");
-            else if(v.getId() == R.id.button29) textView.setText("");
-            else if(v.getId() == R.id.button26) textView.append("+");
-            else if(v.getId() == R.id.button27) textView.append("-");
-            else if(v.getId() == R.id.button25) textView.append("x");
-            else if(v.getId() == R.id.button23) textView.append("/");
-            else if(v.getId() == R.id.button) textView.append("²");
-            else if(v.getId() == R.id.button10) textView.append("√");
-            else if(v.getId() == R.id.button11) textView.append("(");
-            else if(v.getId() == R.id.button12) textView.append(")");
-            else if(v.getId() == R.id.button13) textView.append("%");
-            else if(v.getId() == R.id.button20) textView.append("");
+            if (v.getId() == R.id.button1) textView.append("1");
+            else if (v.getId() == R.id.button2) textView.append("2");
+            else if (v.getId() == R.id.button3) textView.append("3");
+            else if (v.getId() == R.id.button4) textView.append("4");
+            else if (v.getId() == R.id.button5) textView.append("5");
+            else if (v.getId() == R.id.button6) textView.append("6");
+            else if (v.getId() == R.id.button7) textView.append("7");
+            else if (v.getId() == R.id.button8) textView.append("8");
+            else if (v.getId() == R.id.button9) textView.append("9");
+            else if (v.getId() == R.id.button19) textView.append("0");
+            else if (v.getId() == R.id.button31) textView.append(".");
+            else if (v.getId() == R.id.button28) textView.setText("");
+            else if (v.getId() == R.id.button29) textView.setText("");
+            else if (v.getId() == R.id.button26) textView.append("-");
+            else if (v.getId() == R.id.button27) textView.append("+");
+            else if (v.getId() == R.id.button25) textView.append("x");
+            else if (v.getId() == R.id.button23) textView.append("/");
+            else if (v.getId() == R.id.button) textView.append("²");
+            else if (v.getId() == R.id.button10) textView.append("√");
+            else if (v.getId() == R.id.button11) textView.append("(");
+            else if (v.getId() == R.id.button12) textView.append(")");
+            else if (v.getId() == R.id.button13) textView.append("%");
+            else if (v.getId() == R.id.button20) textView.append("");
 
-
-
-    }
-
+        }
 
 }
